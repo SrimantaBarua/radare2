a2f2193edfbcc1b840fbd00551dc1f0589de4b05
Srimanta Barua <srimanta.barua1@gmail.com>
2017-12-05 15:52:56 +0530

    add sp and bp to mips profile

--- shlr/gdb/src/gdbclient/xml.c
+++ shlr/gdb/src/gdbclient/xml.c
@@ -278,7 +278,10 @@ static int gdbr_parse_target_xml(libgdbr_t *g, char *xml_data, ut64 len) {
 		}
 		break;
 	case R_SYS_ARCH_MIPS:
-		if (!(profile = r_str_prefix (profile, "=PC	pc\n"))) {
+		if (!(profile = r_str_prefix (profile,
+						"=PC	pc\n"
+						"=SP	r29\n"
+						"=BP	r28\n"))) {
 			goto exit_err;
 		}
 		break;
